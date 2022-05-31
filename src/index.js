faf13380a055223840f8ddfb1ac5da15c2caf467
Santi Miranda <santimir@protonmail.com>
2022-05-31 21:13:14 +0200

    added prettier and eslint

--- src/index.js
+++ src/index.js
@@ -3,17 +3,24 @@
 import chalk from "chalk";
 import data from "./quotes.js";
 
-const rand = Math.floor(Math.random()*data.length)
+const log = console.log;
+const rand = Math.floor(Math.random() * data.length);
 
-const { quote, author } = data[rand]
+const { quote, author } = data[rand];
 
-// When user executes the CLI with "full" 
-if(process.argv.slice(2,).indexOf("full")!==-1){
-  const date = (new Date()).toDateString()
-  const user = process.env.USERNAME
+// When user executes the CLI with "full"
+if (process.argv.slice(2).indexOf("full") !== -1) {
+  const date = new Date().toDateString();
+  const user = process.env.USERNAME;
 
-  if(user) console.log("\n "+ chalk.bold(user) + " | "+ date )
-  else console.log("\n "+date)
+  if (user) log("\n " + chalk.bold(user) + " | " + date);
+  else log("\n " + date);
 }
 
-console.log("\n "+chalk.italic(quote)+"\n "+chalk.bold.white.bgMagenta(author)+"\n")
+log(
+  "\n " +
+    chalk.italic(quote) +
+    "\n " +
+    chalk.bold.white.bgMagenta(author) +
+    "\n"
+);
